db0fb6721544701171c9c4f280105aed56fc8b68
kurutto <namihojo@gmail.com>
2025-06-29 13:46:29 +0900

    feat:ログアウト機能の実装

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey:process.env.NEXT_PUBLIC_APP_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_APP_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.NEXT_PUBLIC_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_APP_FIREBASE_PROJECT_ID,
@@ -17,24 +22,35 @@ export default app;
 
 const provider = new GoogleAuthProvider();
 export const auth = getAuth();
-export const signInWithGoogle = () => {
+export const logIn = () => {
   signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential?.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
-} 
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};
+
+export const logOut = () => {
+  signOut(auth)
+    .then(() => {
+      document.location.reload();
+    })
+    .catch((err) => {
+      console.log("サインアウトエラー");
+    });
+};
