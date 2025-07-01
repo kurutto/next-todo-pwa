f3cb7272ad828867d543d99fe4a27ed70ac48776
kurutto <namihojo@gmail.com>
2025-07-01 15:18:50 +0900

    fix:firebaseのエラー解消

--- src/app/api/todo/[uid]/[id]/route.ts
+++ src/app/api/todo/[uid]/[id]/route.ts
@@ -1,27 +1,25 @@
 import { db } from "@/lib/firebase";
-import {
-  doc,
-  deleteDoc,
-} from "firebase/firestore";
-import {NextRequest, NextResponse } from "next/server";
+import { doc, deleteDoc } from "firebase/firestore";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(req: NextRequest, { params }: { params: { uid:string;id: string } }) {
-  try{
-    const { id } = await params;
-    console.log("id",id);
-    if(!id){
-      return NextResponse.json(
-        {
-          status: 400,
-          headers: {
-            "Content-Type": "application/json; charset=utf-8",
-          },
-        }
-      );
+export async function DELETE(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  try {
+    const { id } = await context.params;
+    console.log("id", id);
+    if (!id) {
+      return NextResponse.json({
+        status: 400,
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      });
     }
     await deleteDoc(doc(db, "todo", id));
     return NextResponse.json({ status: 201 });
-  }catch(err){
+  } catch (err) {
     console.error("DELETE Error:", err);
     return NextResponse.json(
       { message: "サーバーエラーが発生しました" },
@@ -32,6 +30,5 @@ export async function DELETE(req: NextRequest, { params }: { params: { uid:strin
         },
       }
     );
-
   }
 }
